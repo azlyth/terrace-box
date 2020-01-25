d1c1a0dd2dafbd7319fbc013654fcfff92025ea7
Peter Valdez <peter@nycmesh.net>
2020-01-25 14:59:54 -0500

    Display the episode number and title

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const FRAMES_PER_EPISODE = 50;
+const FRAMES_PER_EPISODE = 65;
 
 let episodes, currentEpisode, lastEpisode, house;
 let members = [];
@@ -12,8 +12,18 @@ function setup() {
   //frameRate(10);
   createCanvas(windowWidth, windowHeight);
 
+  let desktop = true;
+  if (desktop) {
+    dimens = {
+      textSize: 32,
+      topStep: 50,
+    }
+  } else {
+
+  }
+
   // Start off from the 0th episode (so that entrances trigger on the transition to 1)
-  currentEpisode = { number: 0 };
+  currentEpisode = { number: 0, members: [] };
   lastEpisode = currentEpisode;
 
   // Create the house
@@ -45,13 +55,30 @@ function draw() {
   }
 
   // Color the background
-  background(50, 89, 100);
+  background('white');
   house.display();
 
-  // Move and draw the members
-  for (let i = 0; i < members.length; i++) {
-    members[i].move();
-    members[i].display();
+  // Draw the title
+  if (int(currentEpisode.number) > 0) {
+    textSize(dimens.textSize);
+    textStyle(NORMAL);
+    textAlign(CENTER);
+    text('Episode ' + currentEpisode.number, width / 2, dimens.topStep * 2);
+
+    textStyle(ITALIC);
+    text(currentEpisode.title, width / 2, 150);
+
+    // Move and draw the members
+    for (let i = 0; i < members.length; i++) {
+      members[i].move();
+      members[i].display();
+    }
+  } else {
+    // Display the title
+    textSize(50);
+    textStyle(NORMAL);
+    textAlign(CENTER);
+    text('TERRACE BOX', width / 2, height / 2);
   }
 }
 
@@ -84,10 +111,12 @@ class Member {
   constructor(name) {
     this.name = name;
 
+    // Initialize characteristics
     this.speed = 3;
     this.diameter = random(15, 30);
     this.radius = this.diameter / 2;
 
+    // Initialize position in the house
     this.x = random(house.topLeft.x + this.radius,
                     house.topLeft.x - this.radius + house.width);
     this.y = random(house.topLeft.y + this.radius,
@@ -98,6 +127,7 @@ class Member {
     let new_x = this.x + random(-this.speed, this.speed);
     let new_y = this.y + random(-this.speed, this.speed);
 
+    // Only move to the new position if its in the house
     if (house.contains(new_x, new_y, this.radius)) {
       this.x = new_x;
       this.y = new_y;
